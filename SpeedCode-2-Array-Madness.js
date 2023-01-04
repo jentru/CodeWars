b4af1a40a0b8749e6889be3a501ee043b17b7838
Jenny Truong <jennytruong1995@gmail.com>
2023-01-04 15:34:44 -0600

    Coding challenge for 01/04/23

---
+++ SpeedCode-2-Array-Madness.js
@@ -0,0 +1,12 @@
+//Description
+// Given two integer arrays a, b, both of length >= 1, create a program that returns true if the sum of the squares of each element in a is strictly greater than the sum of the cubes of each element in b.
+
+// E.g.
+
+// arrayMadness([4, 5, 6], [1, 2, 3]); // returns true since 4 ** 2 + 5 ** 2 + 6 ** 2 > 1 ** 3 + 2 ** 3 + 3 ** 3
+// Get your timer out. Are you ready? Ready, get set, GO!!!
+
+function arrayMadness(a, b) {
+    return a.reduce((sum, current)=> sum+ current**2, 0) > b.reduce((sum,current)=> sum+current**3,0)? true:false;
+    
+}
