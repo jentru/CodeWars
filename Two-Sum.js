aa4eee2f5504040cc8f4b71108473bf7ae41d83a
Jenny Truong <jennytruong1995@gmail.com>
2023-01-27 18:57:59 -0600

    Coding challenge for 01/27/23

---
+++ Two-Sum.js
@@ -0,0 +1,13 @@
+//Description
+
+function twoSum(numbers, target) {
+    for (let i=0; i<numbers.length; i++){
+      for (let j=0; j<numbers.length; j++){
+        if(i !== j){
+          if(numbers[i]+numbers[j] == target){
+            return [i, j];
+          }
+        }
+      }
+    }
+}
