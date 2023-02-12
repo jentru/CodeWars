eb848079fe0191f69d501e1e9cfc33995728c205
Jenny Truong <jennytruong1995@gmail.com>
2023-02-12 15:19:06 -0600

    Coding challenge for 02/12/23

---
+++ Two-to-One.js
@@ -0,0 +1,15 @@
+//Description
+// Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+
+// Examples:
+// a = "xyaabbbccccdefww"
+// b = "xxxxyyyyabklmopq"
+// longest(a, b) -> "abcdefklmopqwxy"
+
+// a = "abcdefghijklmnopqrstuvwxyz"
+// longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+function longest(s1, s2) {
+    let total = s1+s2;
+    return total.split("").sort().filter((e,i,a)=> e !== a[i+1]).join("");
+}
