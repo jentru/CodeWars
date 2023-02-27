9d8dbaf19de7fbc9a75145de29b31a44a781bfec
jentru <114699983+jentru@users.noreply.github.com>
2023-02-27 16:24:34 -0600

    Create count-the-divisors-of-s-number.js

---
+++ count-the-divisors-of-s-number.js
@@ -0,0 +1,11 @@
+//Description
+//Count the number of divisors of a positive integer n.
+function getDivisorsCnt(n){
+    let count = 0;
+  for (let i=1; i<=n; i++){
+    if (n%i==0){
+      count++;
+    }
+  }
+  return count;
+}
