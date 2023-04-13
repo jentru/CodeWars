3158562d34c6880e38f2247784349e5111953519
Jenny Truong <jennytruong1995@gmail.com>
2023-04-13 13:15:00 -0500

    Coding challenge for 04/13/23

---
+++ Find-the-position.js
@@ -0,0 +1,10 @@
+//Description
+// When provided with a letter, return its position in the alphabet.
+
+// Input :: "a"
+
+// Ouput :: "Position of alphabet: 1"
+function position(letter){
+    const alphabet = ' abcdefghijklmnopqrstuvwxyz';
+    return 'Position of alphabet: ' +alphabet.indexOf(letter.toLowerCase());
+}
