25108f69d8c1976bbb9485d87a142cbd04da6db9
Jenny Truong <jennytruong1995@gmail.com>
2023-04-29 18:42:30 -0500

    Coding challenge for 04/29/23

---
+++ Product-of-consecutive-Fib-numbers.js
@@ -0,0 +1,61 @@
+// //Description
+// The Fibonacci numbers are the numbers in the following integer sequence (Fn):
+
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, ...
+
+// such as
+
+// F(n) = F(n-1) + F(n-2) with F(0) = 0 and F(1) = 1.
+
+// Given a number, say prod (for product), we search two Fibonacci numbers F(n) and F(n+1) verifying
+
+// F(n) * F(n+1) = prod.
+
+// Your function productFib takes an integer (prod) and returns an array:
+
+// [F(n), F(n+1), true] or {F(n), F(n+1), 1} or (F(n), F(n+1), True)
+// depending on the language if F(n) * F(n+1) = prod.
+
+// If you don't find two consecutive F(n) verifying F(n) * F(n+1) = prodyou will return
+
+// [F(n), F(n+1), false] or {F(n), F(n+1), 0} or (F(n), F(n+1), False)
+// F(n) being the smallest one such as F(n) * F(n+1) > prod.
+
+// Some Examples of Return:
+// (depend on the language)
+
+// productFib(714) # should return (21, 34, true), 
+//                 # since F(8) = 21, F(9) = 34 and 714 = 21 * 34
+
+// productFib(800) # should return (34, 55, false), 
+//                 # since F(8) = 21, F(9) = 34, F(10) = 55 and 21 * 34 < 800 < 34 * 55
+// -----
+// productFib(714) # should return [21, 34, true], 
+// productFib(800) # should return [34, 55, false], 
+// -----
+// productFib(714) # should return {21, 34, 1}, 
+// productFib(800) # should return {34, 55, 0},        
+// -----
+// productFib(714) # should return {21, 34, true}, 
+// productFib(800) # should return {34, 55, false}, 
+function productFib(prod){
+    //create the Fibonacci numbers
+    let arr = [0,1];
+    let i=1;
+    while (arr[i]<= prod/arr[i]){
+      arr.push(arr[i-1]+arr[i]);
+      i++;
+    }
+    //determine the product numbers based on the previously created array
+    if (arr[i-1] == prod/arr[i]){
+      return [arr[i-1],arr[i],true];
+    } else if (arr[i-1]*arr[i] > prod){
+      return [arr[i-1],arr[i],false];
+    } else {
+      arr.push(arr[i-1]+arr[i]);
+    // need to add another index to be called 
+      i++;
+      return [arr[i-1],arr[i],false];
+    }
+    
+}
