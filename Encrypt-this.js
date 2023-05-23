c6abb18de01e9be1b3823d3d2263af00d815abf8
Jenny Truong <jennytruong1995@gmail.com>
2023-05-23 15:24:40 -0500

    Coding challenge for 05/23/23

---
+++ Encrypt-this.js
@@ -0,0 +1,21 @@
+//Description
+// Encrypt this!
+
+// You want to create secret messages which can be deciphered by the Decipher this! kata. Here are the conditions:
+
+// Your message is a string containing space separated words.
+// You need to encrypt each word in the message using the following rules:
+// The first letter must be converted to its ASCII code.
+// The second letter must be switched with the last letter
+// Keepin' it simple: There are no special characters in the input.
+// Examples:
+// encryptThis("Hello") === "72olle"
+// encryptThis("good") === "103doo"
+// encryptThis("hello world") === "104olle 119drlo"
+
+var encryptThis = function(text) {
+    let encrypt = text.split(' ').map((word,i,text)=> word.length>2? word.charCodeAt(0)+word[word.length-1]+word.slice(2,word.length-1)+word[1] : 
+    word.length==2? word.charCodeAt(0)+word.slice(1) : 
+    word.charCodeAt(0)).join(' ')
+    return encrypt
+}
