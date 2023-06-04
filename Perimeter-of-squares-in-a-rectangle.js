c788e35ecc744ead624e44166b6fe2f2d58bc0e9
Jenny Truong <jennytruong1995@gmail.com>
2023-06-04 14:54:25 -0500

    Coding challenge for 06/04/23

---
+++ Perimeter-of-squares-in-a-rectangle.js
@@ -0,0 +1,29 @@
+//Description
+// The drawing shows 6 squares the sides of which have a length of 1, 1, 2, 3, 5, 8. It's easy to see that the sum of the perimeters of these squares is : 4 * (1 + 1 + 2 + 3 + 5 + 8) = 4 * 20 = 80 
+
+// Could you give the sum of the perimeters of all the squares in a rectangle when there are n + 1 squares disposed in the same manner as in the drawing:
+
+// alternative text
+
+// Hint:
+// See Fibonacci sequence
+
+// Ref:
+// http://oeis.org/A000045
+
+// The function perimeter has for parameter n where n + 1 is the number of squares (they are numbered from 0 to n) and returns the total perimeter of all the squares.
+
+// perimeter(5)  should return 80
+// perimeter(7)  should return 216
+
+function perimeter(n) {
+  let sum = 0
+  let sq = [1,1]
+    for (let i=0; i<n+1; i++){
+      sum += 4*sq[i]
+      if (i>=1){
+        sq.push(sq[i]+sq[i-1])
+      }
+    }
+  return sum
+}
