e16d9b7eb88ea0b0855010153886ca3f3398e1a0
Jenny Truong <jennytruong1995@gmail.com>
2023-06-24 23:51:30 -0500

    Coding challenge for 06/24/23

---
+++ Who-has-the-most-money.js
@@ -0,0 +1,26 @@
+//Description
+// You're going on a trip with some students and it's up to you to keep track of how much money each Student has. A student is defined like this:
+
+// class Student {
+//   constructor(name, fives, tens, twenties) {
+//     this.name = name;
+//     this.fives = fives;
+//     this.tens = tens;
+//     this.twenties = twenties;
+//   }
+// }
+// As you can tell, each Student has some fives, tens, and twenties. Your job is to return the name of the student with the most money. If every student has the same amount, then return "all".
+
+// Notes:
+
+// Each student will have a unique name
+// There will always be a clear winner: either one person has the most, or everyone has the same amount
+// If there is only one student, then that student has the most money
+
+Student.prototype.moneyTotal = function (){
+    return this.fives*5 + this.tens*10 + this.twenties*20
+}
+function mostMoney(students) {
+    let maxMoney = students.sort((a,b)=> b.moneyTotal()-a.moneyTotal())[0].moneyTotal()
+    return students.filter(el => el.moneyTotal() == maxMoney).length == 1 ? students[0].name : 'all'
+}
