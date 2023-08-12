72c0e9bf3c0190721e696b3fd7c33663d9b98cd1
Jenny Truong <jennytruong1995@gmail.com>
2023-08-12 16:01:01 -0500

    Coding challenge for 08/12/23

---
+++ Reverse-a-Number.js
@@ -0,0 +1,14 @@
+//Description
+// Given a number, write a function to output its reverse digits. (e.g. given 123 the answer is 321)
+
+// Numbers should preserve their sign; i.e. a negative number should still be negative when reversed.
+
+// Examples
+//  123 ->  321
+// -456 -> -654
+// 1000 ->    1
+
+
+function reverseNumber(n) {
+    return n>=0? Number(n.toString().split('').reverse().join('')) : Number((n*-1).toString().split('').reverse().join(''))*-1
+}
