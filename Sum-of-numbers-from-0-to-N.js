2e83b8376de53fdf221318f07b556d2164409421
Jenny Truong <jennytruong1995@gmail.com>
2023-08-28 22:08:31 -0500

    Coding challenge for 08/28/23

---
+++ Sum-of-numbers-from-0-to-N.js
@@ -0,0 +1,43 @@
+//Description
+// We want to generate a function that computes the series starting from 0 and ending until the given number.
+
+// Example:
+// Input:
+
+// > 6
+// Output:
+
+// 0+1+2+3+4+5+6 = 21
+
+// Input:
+
+// > -15
+// Output:
+
+// -15<0
+
+// Input:
+
+// > 0
+// Output:
+
+// 0=0
+
+var SequenceSum = (function() {
+    function SequenceSum() {}
+  
+    SequenceSum.showSequence = function(count) {
+      let sumString ='0'
+      let sum = 0
+      if(count ==0) {return `0=0`}
+      if(count < 0) {return `${count}<0`}
+      for(let i=1;i<=count;i++){
+        sumString+=`+${i}`
+        sum +=i
+      }
+      return `${sumString} = ${sum}`
+    };
+  
+    return SequenceSum;
+  
+  })();
