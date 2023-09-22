caead97e6e42a3b9dafce994c830ed53a7fd6f9a
Jenny Truong <jennytruong1995@gmail.com>
2023-09-21 19:15:47 -0500

    Coding challenge for 09/21/23

---
+++ Difference-of-2.js
@@ -0,0 +1,21 @@
+//Description
+// The objective is to return all pairs of integers from a given array of integers that have a difference of 2.
+
+// The result array should be sorted in ascending order of values.
+
+// Assume there are no duplicate integers in the array. The order of the integers in the input array should not matter.
+
+// Examples
+// [1, 2, 3, 4]  should return [[1, 3], [2, 4]]
+
+// [4, 1, 2, 3]  should also return [[1, 3], [2, 4]]
+
+// [1, 23, 3, 4, 7] should return [[1, 3]]
+
+// [4, 3, 1, 5, 6] should return [[1, 3], [3, 5], [4, 6]]
+function twosDifference(input){
+    let dif =[]
+    input.sort((a,b)=> a-b).map((el,i,arr)=> 
+      arr.find(num=> num==el+2)>0? dif.push([el,arr.find(num=> num==el+2)]) : '')
+    return dif
+}
