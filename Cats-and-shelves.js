210d8e6dbf271a8327a74d2971f40143059f975a
Jenny Truong <jennytruong1995@gmail.com>
2023-11-17 00:49:21 -0600

    Coding challenge for 11/7/23

---
+++ Cats-and-shelves.js
@@ -0,0 +1,38 @@
+//Description
+// An infinite number of shelves are arranged one above the other in a staggered fashion.
+// The cat can jump either one or three shelves at a time: from shelf i to shelf i+1 or i+3 (the cat cannot climb on the shelf directly above its head), according to the illustration:
+
+//                  ┌────────┐
+//                  │-6------│
+//                  └────────┘
+// ┌────────┐       
+// │------5-│        
+// └────────┘  ┌─────► OK!
+//             │    ┌────────┐
+//             │    │-4------│
+//             │    └────────┘
+// ┌────────┐  │
+// │------3-│  │     
+// BANG!────┘  ├─────► OK! 
+//   ▲  |\_/|  │    ┌────────┐
+//   │ ("^-^)  │    │-2------│
+//   │ )   (   │    └────────┘
+// ┌─┴─┴───┴┬──┘
+// │------1-│
+// └────────┘
+// Input
+// Start and finish shelf numbers (always positive integers, finish no smaller than start)
+
+// Task
+// Find the minimum number of jumps to go from start to finish
+
+// Example
+// Start 1, finish 5, then answer is 2 (1 => 4 => 5 or 1 => 2 => 5)
+function solution(start, finish) 
+{
+  let count =0 
+  let total = finish-start
+  count += parseInt(total/3)
+  count += parseInt(total%3)
+  return count
+}
