377d5120c1b918883bfce7ddc75c191181e2632f
Jenny Truong <jennytruong1995@gmail.com>
2023-12-13 15:35:47 -0600

    Coding challenge for 12/13/23

---
+++ Alphabetical-Addition.js
@@ -0,0 +1,25 @@
+// Description
+// Your task is to add up letters to one letter.
+
+// The function will be given a variable amount of arguments, each one being a letter to add.
+
+// Notes:
+// Letters will always be lowercase.
+// Letters can overflow (see second to last example of the description)
+// If no letters are given, the function should return 'z'
+// Examples:
+// addLetters('a', 'b', 'c') = 'f'
+// addLetters('a', 'b') = 'c'
+// addLetters('z') = 'z'
+// addLetters('z', 'a') = 'a'
+// addLetters('y', 'c', 'b') = 'd' // notice the letters overflowing
+// addLetters() = 'z'
+
+function addLetters(...letters) {
+    let alpha = 'zabcdefghijklmnopqrstuvwxy'
+    if (letters == false){
+        return 'z'
+    }
+    let i = letters.map(ltr=> alpha.indexOf(ltr)).reduce((acc,cur)=> acc+cur)%26 
+    return alpha[i]
+}
