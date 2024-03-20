ce5191451cfc44c311ef9b949d5fef8d0aa8f9c4
Jenny Truong <jennytruong1995@gmail.com>
2024-03-20 17:28:14 -0500

    Coding challenge for 03/20/24

---
+++ Numbers-to-letters.js
@@ -0,0 +1,7 @@
+// Description
+// Given an array of numbers (in string format), you must return a string. The numbers correspond to the letters of the alphabet in reverse order: a=26, z=1 etc. You should also account for '!', '?' and ' ' that are represented by '27', '28' and '29' respectively.
+
+// All inputs will be valid.
+function switcher(x){
+    return x.map(n=> n<27? String.fromCharCode(123- +n): '!? '[n-27]).join('')
+}
