400d09e0ef7b731d68171e0bba1efdc4d9436b28
Jenny Truong <jennytruong1995@gmail.com>
2024-03-27 17:18:17 -0500

    Coding challenge for 03/27/24

---
+++ Return-the-first-M-multiples-of-N.js
@@ -0,0 +1,13 @@
+// Description
+// Implement a function, multiples(m, n), which returns an array of the first m multiples of the real number n. Assume that m is a positive integer.
+
+// Ex.
+
+// multiples(3, 5.0)
+// should return
+
+// [5.0, 10.0, 15.0]
+
+function multiples(m, n){
+    return new Array(m).fill(1).map((el,i)=> (el+i)*n )
+}
