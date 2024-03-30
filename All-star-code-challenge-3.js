f8b74f47435e879fb054cae9b7867294f2826ba5
Jenny Truong <jennytruong1995@gmail.com>
2024-03-30 18:24:26 -0500

    Coding challenge for 03/30/24

---
+++ All-star-code-challenge-3.js
@@ -0,0 +1,13 @@
+// Description
+// Create a function that takes a string argument and returns that same string with all vowels removed (vowels are "a", "e", "i", "o", "u").
+
+// Example (Input --> Output)
+
+// "drake" --> "drk"
+// "aeiou" --> ""
+// remove_vowels("drake") // => "drk"
+// remove_vowels("aeiou") // => ""
+
+var removeVowels = function(str){
+    return str.replace(/[aeiou]/ig,'')
+}
