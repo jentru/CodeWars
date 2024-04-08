a1ec97a9fabbe95abfd04d3cf15f177977c70f72
Jenny Truong <jennytruong1995@gmail.com>
2024-04-08 16:23:46 -0500

    Coding challenge for 04/08/24

---
+++ How-many-arguments.js
@@ -0,0 +1,10 @@
+// Description
+// Write a function that returns the number of arguments it received.
+
+// args_count() --> 0
+// args_count('a') --> 1
+// args_count('a', 'b') --> 2
+
+function args_count() {
+    return arguments.length
+}
