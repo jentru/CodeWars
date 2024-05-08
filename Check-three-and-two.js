27d79a3dd5a1e6dee98a5e0a8f59cbf012347969
Jenny Truong <jennytruong1995@gmail.com>
2024-05-08 15:25:59 -0500

    Coding challenge for 05/08/24

---
+++ Check-three-and-two.js
@@ -0,0 +1,12 @@
+// Description
+// Given an array with exactly 5 strings "a", "b" or "c" (chars in Java, characters in Fortran), check if the array contains three and two of the same values.
+
+// Examples
+// ["a", "a", "a", "b", "b"] ==> true  // 3x "a" and 2x "b"
+// ["a", "b", "c", "b", "c"] ==> false // 1x "a", 2x "b" and 2x "c"
+// ["a", "a", "a", "a", "a"] ==> false // 5x "a"
+
+function checkThreeAndTwo(array) {
+    let count = ['a','b','c'].map(char=> array.filter(el=> el==char).length)
+    return count.includes(2) && count.includes(3)
+}
